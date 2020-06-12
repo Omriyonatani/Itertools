2b9c2ceb9f75c546352c9a480c5eed66ef87113b
kokole130 <kokole130@gmail.com>
2020-06-12 19:09:12 +0300

    test starting

--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,24 @@
+#include "doctest.h"
+#include "accumulate.hpp"
+#include "compress.hpp"
+#include "filterfalse.hpp"
+#include "range.hpp"
+
+using namespace std;
+
 struct divide4{
     bool operator()(int i){
         return i%4!=0;
     }
 };
+
+struct lessThan3 {
+    bool operator()(int i) const { return i < 3; }
+};
+
+TEST_CASE("Everything"){
+
+}
+
+
+
