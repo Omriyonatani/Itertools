ac0fe1044c3762aa5cdbeca94859c88c63638192
kokole130 <kokole130@gmail.com>
2020-06-13 21:52:04 +0300

    tests

--- Test.cpp
+++ Test.cpp
@@ -4,6 +4,7 @@
 #include "filterfalse.hpp"
 #include "range.hpp"
 
+using namespace itertools;
 using namespace std;
 
 struct divide4{
@@ -15,9 +16,18 @@ struct divide4{
 struct lessThan3 {
     bool operator()(int i) const { return i < 3; }
 };
-
+/*
+range(5,9)
+accumulate(range(5,9))
+accumulate(vecString)
+accumulate(range(5,9), [](int x, int y){return x*y;})
+filterfalse(lessThan3{}, vecInt)
+filterfalse([](int i){return i%2==0;}, range(5,9))
+compress(string("abcd"), vector<bool>({true,true,false,true}))
+compress(range(5,9), vector<bool>({true,true,false,true}))
+*/
 TEST_CASE("Everything"){
-
+    CHECK(range(5,9) == (5,6,7,8));
 }
 
 
