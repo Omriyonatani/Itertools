0850eaabc49a7238091767eda2729a14498b0450
omriyonatani <omriyonatani@gmail.com>
2020-06-15 01:50:22 +0300

    Tests

--- Test.cpp
+++ Test.cpp
@@ -12,14 +12,18 @@
 using namespace itertools;
 using namespace std;
 
-struct divide4{
-    bool operator()(int i){
-        return i%4!=0;
+struct divide4 {
+    bool operator()(int i) {
+        return i % 4 != 0;
     }
 };
 
 struct lessThan3 {
     bool operator()(int i) const { return i < 3; }
+};
+
+struct lessThan2 {
+    bool operator()(int i) const { return i < 2; }
 };
 /*
 range(5,9)
@@ -36,69 +40,79 @@ compress(range(5,9), vector<bool>({true,true,false,true}))
 // }
 
 
-TEST_CASE("range checks"){
-    int num=5;
-    for(auto i:range(5,9)){
-        CHECK(i==num);
+TEST_CASE ("range checks") {
+    int num = 5;
+    for (auto i:range(5, 9)) {
+                CHECK(i == num);
         num++;
     }
 
-    num=-7;
-    for(auto i:range(-7,3)){
-        CHECK(i==num);
+    num = -7;
+    for (auto i:range(-7, 3)) {
+                CHECK(i == num);
         num++;
     }
 
-    range t(5,5);
-    CHECK(t.begin()==t.end());
-    CHECK_NOTHROW(range(2,10));
-    CHECK_NOTHROW(range(2,2));
-    CHECK_THROWS(range(2,1));
-    CHECK_THROWS(range(5,-5));
+    range t(5, 5);
+    CHECK(t.begin() == t.end());
+    CHECK_NOTHROW(range(2, 10));
+    CHECK_NOTHROW(range(2, 2));
+    CHECK_THROWS(range(2, 1));
+    CHECK_THROWS(range(5, -5));
 }
 
-TEST_CASE("accumulate checks"){
-    vector<int> vecInt = {1,2,3,4};
-    vector<string> vecString = {"Hello", "Bye", "Adam"};
+TEST_CASE ("accumulate checks") {
+    vector<int> vecInt = {1, 2, 3, 4};
+    vector <string> vecString = {"Hello", "Bye", "Adam"};
     vector<float> vecFloat = {-1, 0.3, 5.2, -8.3};
 
 
-    
-    int num=0;
-	for (int i: accumulate(range(5,9))){
-        num+=i-num;
-        CHECK(i==num); // 5 11 18 26   
-    }
-       
-		
-         
-    string s="";
-    int len=0;
-    for (auto i: accumulate(vecString) ){
-        len=i.length();
-        s+=i.substr(s.length(),len);
-        CHECK(i==s);// Hello HelloBye HelloByeAdam
-    }
-        
-
-    num=1;
-	for (int i: accumulate(range(5,9), [](int x, int y){return x*y;})){
-        num*=i/num;
-		CHECK(i==num);      // 5 30 210 1680
+    int num = 0;
+    for (int i: accumulate(range(5, 9))) {
+        num += i - num;
+                CHECK(i == num); // 5 11 18 26
     }
 
 
-
-    for (int i: accumulate(deque,[](int x,int y){return x*y;})){
-        cout <<"i="<<i<<endl;      // 5 30 210 1680
+    string s = "";
+    int len = 0;
+    for (auto i: accumulate(vecString)) {
+        len = i.length();
+        s += i.substr(s.length(), len);
+                CHECK(i == s);// Hello HelloBye HelloByeAdam
     }
 
 
-        
+    num = 1;
+    for (int i: accumulate(range(5, 9), [](int x, int y) { return x * y; })) {
+        num *= i / num;
+                CHECK(i == num);      // 5 30 210 1680
+    }
+
+
+//    for (int i: accumulate(deque, [](int x, int y) { return x * y; })) {
+//        cout << "i=" << i << endl;      // 5 30 210 1680
+//    }
 
 }
 
+TEST_CASE ("filterfalse checks") {
+    vector<int> vecInt = {1, 2, 2, 0};
+    vector<int> vecInt2 = {5, 6, 7, 8};
+    vector<int> vecInt3 = {2, 2, 2, 2};
+    vector<int> vecInt4 = {-1, 2, -2, 4};
 
+
+
+}
+
+TEST_CASE ("compress checks") {
+
+}
+
+TEST_CASE ("random check") {
+
+}
 
 
 
